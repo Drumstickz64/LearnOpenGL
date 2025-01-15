ba81c68aa84543637fb26c0eebc0466eeb5cd33a
Drumstickz64 <gearman533@gmail.com>
2025-01-16 00:53:25 +0300

    finish chapter "shaders"

---
+++ LearnOpenGL/shader_program.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <glad/glad.h> // include glad to get all the required OpenGL headers
+
+#include <string>
+
+class Shader_Program
+{
+public:
+    // the program ID
+    GLuint id;
+
+    // constructor reads and builds the shader
+    Shader_Program(const std::string& vertexSource, const std::string& fragmentSource);
+    // use/activate the shader
+    void use() const;
+    // utility uniform functions
+    void set_bool(const std::string& name, bool value) const;
+    void set_int(const std::string& name, GLint value) const;
+    void set_float(const std::string& name, GLfloat value) const;
+};
